46a420cddd0a1434e362d206161444062ef52b61
hbgyeom <hbgyeom@khu.ac.kr>
2025-05-22 18:40:24 +0900

    1

--- pycodes/filter.py
+++ pycodes/filter.py
@@ -5,5 +5,9 @@ import os
 def filter(input_file):
     input_path = os.path.join("csv_data", input_file)
     print(input_path)
+    try:
+        df = read_csv(input_path)
+    except Exception:
+        print(f"Failed: {input_path}")
 
 filter("hn20_all.csv")
--- pycodes/sas2csv.py
+++ pycodes/sas2csv.py
@@ -5,10 +5,14 @@ os.makedirs("csv_data", exist_ok=True)
 
 for f in os.listdir("sas_data"):
     if f.endswith(".sas7bdat"):
+        input_path = os.path.join("sas_data", f)
+        csv_name = f.replace(".sas7bdat", ".csv")
+        output_path = os.path.join("csv_data", csv_name)
+
         try:
-            df, _ = pyreadstat.read_sas7bdat(f"sas_data/{f}")
-            csv_name = f.replace(".sas7bdat", ".csv")
-            df.to_csv(f"csv_data/{csv_name}", index=False, encoding="utf-8")
-            print(f"{f} -> {csv_name}")
+            df, _ = pyreadstat.read_sas7bdat(input_path)
+            df.to_csv(output_path, index=False, encoding="utf-8")
+            print(f"Converted: {csv_name}")
+
         except Exception:
-            print(f"Failed {f}")
+            print(f"Failed: {f}")
